ee6e3ed0290a7c1fc5b62c3ea1b24e48e0c29962
thesullivantage <38443606+thesullivantage@users.noreply.github.com>
2024-01-22 17:13:17 -0500

    prune

--- model_pk/loss.py
+++ model_pk/loss.py
@@ -18,12 +18,12 @@ class LossODE(object):
     ### get different functions 
     ### MARK DEV: issue with 
     
-    def approx_eval(self, subclassed_model, input):
+    def approx_eval(self, model, input):
         '''
         MARK DEV: DIMENSION FINE
         x*NN(x) + f_0
         '''
-        return subclassed_model(input) + self.initial
+        return model(input) + self.initial
 
     def ode_analy(self, input, funcKey = 'place_h'):
         '''
@@ -31,7 +31,7 @@ class LossODE(object):
         MARK DEV: Probably a better way to do this: cls.dict_of_functs
         '''
         if funcKey == 'place_h':
-            return 2*input
+            return 2*input + self.initial
         
     def compute_loss_element(self, model, x):
         '''
@@ -43,20 +43,3 @@ class LossODE(object):
         ### derivative (via linearization): del_val approach 0
         NN_deriv = (self.approx_eval(model, val + self.dell) - self.approx_eval(model, val)) / self.dell
         return tf.math.square(NN_deriv - self.ode_analy(val))
-    
-    # def custom_de_loss(self, inputs, model):
-    #     def compute_loss_element(x):
-    #         '''
-    #         ### MARK DEV: PDE: multi-var tuple instead of x; pass dimensionality too
-    #         Function to be mapped to each element of tensor of inputs to get loss element.
-    #         '''
-    #         dNNdt = (self.approx_eval(model, x + self.dell) - self.approx_eval(model, x)) / self.dell
-    #         return tf.math.square(dNNdt - self.ode_analy(x))
-
-    #     # Apply the modular (map) function to each element of the input tensor
-    #     ### MARK DEV: for multi-var tuple (PDE), map across multiple axes
-    #     loss_elements = tf.map_fn(compute_loss_element, inputs, dtype=tf.float32)
-
-    #     # Compute the mean of the loss elements
-    #     # return tf.sqrt(tf.reduce_mean(tf.abs(loss_elements)))
-    #     return tf.math.sqrt(tf.math.reduce_sum(tf.abs(loss_elements)))
