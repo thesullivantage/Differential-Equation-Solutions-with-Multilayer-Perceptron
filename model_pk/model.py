06b15c875d4507ccb9ad6c1df619bd6c0182fa25
thesullivantage <38443606+thesullivantage@users.noreply.github.com>
2024-01-25 16:15:35 -0500

    updated activation function proposals

--- model_pk/model.py
+++ model_pk/model.py
@@ -11,7 +11,8 @@ class FeedForward(keras.layers.Layer):
     '''
     
     
-    def __init__(self, input_dim=1, output_dim=32, activation='sigmoid', output_fromLog=False):
+    def __init__(self, input_dim=1, output_dim=32, out=False, output_fromLog=False):
+        # activation='relu',
         super().__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
@@ -20,10 +21,12 @@ class FeedForward(keras.layers.Layer):
         # probably won't be necessary given larger context of our model - in our calculations of gradients semi-analytically
         # but including for customizability
         self.logit_output = output_fromLog
-        if activation == 'sigmoid':
-            self.activation = tf.nn.sigmoid
+        if not out:
+            # ReLu activation function for hidden layers
+            self.activation = tf.nn.relu
         else:
-            raise ValueError('Error: Activation function passed not supported; please try again.')
+            # Sigmoid activation function for hidden layers
+            self.activation = tf.nn.sigmoid
     
     def build(self, input_shape):
         '''
@@ -56,16 +59,16 @@ class MLP(keras.Model):
         
         for l in range(num_embed_layer):
             if l == 0:
-                # expand from differential equation's physical dimension to embedded dimension
-                lay = FeedForward(input_dim=phys_dimension, output_dim=embed_dim)
+                # HIDDEN: expand from differential equation's physical dimension to embedded dimension
+                lay = FeedForward(input_dim=phys_dimension, output_dim=embed_dim, out=False)
                 self.dense_layers.append(lay)
             elif l == num_embed_layer - 1:
-                # collapse from embedded dimension to physical dimension (of differential equation's physical dimension)
-                lay = FeedForward(input_dim=embed_dim, output_dim=phys_dimension)
+                # OUTPUT: collapse from embedded dimension to physical dimension (of differential equation's physical dimension)
+                lay = FeedForward(input_dim=embed_dim, output_dim=phys_dimension, out=True)
                 self.dense_layers.append(lay)
             else:
-                # continue progressing through model graph in embedded dimension
-                lay = FeedForward(input_dim=embed_dim, output_dim=embed_dim)
+                # HIDDEN: continue progressing through model graph in embedded dimension
+                lay = FeedForward(input_dim=embed_dim, output_dim=embed_dim, out=False)
                 self.dense_layers.append(lay)
         
     def call(self, inputs):
